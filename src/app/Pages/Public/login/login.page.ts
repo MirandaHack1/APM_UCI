d40d8840f2a832ae7d997abed779b47befcd34d2
Cristian Defaz <cristiandefaz191@gmail.com>
2024-09-03 00:48:20 -0500

    Funcionalidad login html y ts carpeta login

--- src/app/Pages/Public/login/login.page.ts
+++ src/app/Pages/Public/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { AuthService } from 'src/app/Services/auth/auth.service';
+import { PasswordRecoveryPage } from '../password-recovery/password-recovery.page';
+import { UserRegisterPage } from '../user-register/user-register.page';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+
+  USAD_EMAIL: string="";
+  USAD_PASSWORD:string="";
+
+  constructor(
+    public servicio:AuthService,
+    public modalCtrl:ModalController,
+    public navCtrl:NavController
+  ) {}
 
   ngOnInit() {}
+  //----------------login-----------------------------
   login() {
-    // Aquí puedes agregar la lógica de autenticación
-    console.log('Intento de inicio de sesión');
+    let datos={
+      accion:'loggin',
+      USAD_EMAIL: this.USAD_EMAIL,
+      USAD_PASSWORD: this.USAD_PASSWORD
+    }
+    this.servicio.postData(datos).subscribe((res:any)=>{
+      if(res.estado==true)
+      {
+       // this.servicio.createSession('USAD_CODE',res.persona[0].codigo);
+       // this.servicio.createSession('USAD_USERNAME',res.persona[0].nombre);
+        this.servicio.showToast(res.mensaje);
+        this.navCtrl.navigateRoot(['/home']);
+      }
+      else{
+        this.servicio.showToast(res.mensaje);
+      }
+    })
   }
+
 }
