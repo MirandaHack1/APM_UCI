438005b257c4a9bd13bca52cc34f6e3575a7731e
Cristian Defaz <cristiandefaz191@gmail.com>
2024-09-05 14:29:07 -0500

    Temas de aplicativo funcional se modifico home,homets ,globalscss y variablesscss

--- src/app/Pages/Secure/home/home.page.ts
+++ src/app/Pages/Secure/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { personCircle, personCircleOutline, sunny, sunnyOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  paletteToggle = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ personCircle, personCircleOutline, sunny, sunnyOutline });
   }
 
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.initializeDarkPalette(prefersDark.matches);
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+  }
+
+  initializeDarkPalette(isDark: boolean) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  }
+
+  toggleChange(ev: any) {
+    this.toggleDarkPalette(ev.detail.checked);
+  }
+
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+  }
 }
