424fb041c40fda5e948bcd7b99fb6813358c3411
DevJhonny <miranda3791167@gmail.com>
2024-09-05 20:37:01 -0500

    CREANDO LA VISTA DE REGISTRAR

--- src/app/Pages/Public/user-register/user-register.page.ts
+++ src/app/Pages/Public/user-register/user-register.page.ts
@@ -6,10 +6,133 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-register.page.scss'],
 })
 export class UserRegisterPage implements OnInit {
+  // DECLARANDO VARIABLES PARA EL FORMULARIO DE INFORMACION DEL CLIENTE
+  firstName: string = '';
+  lastName: string = '';
+  cardNumber: string = '';
+  passportNumber: string = '';
+  phoneNumber: string = '';
+  address: string = '';
+  city: string = '';
+  province: string = '';
+  career: string = '';
+  semester: string = '';
+  age: number = 0;
+  gender: string = '';
+  weight: number = 0;
+  height: number = 0;
+  institutionalEmail: string = '';
+  dateOfBirth: string = '';
+  sede: string = '';
+  isForeigner: boolean = false;
 
-  constructor() { }
+  // DECLARANDO VAIRABLES PARA INICIAR SESION DEL CLIENTE
+  email_user_re: string = '';
+  password_user2: string = '';
+  password_user: string = '';
+  email_user: string = '';
+  user_name: string = '';
+  // Variable para manejar la visibilidad de la contraseña
+  showPassword: boolean = false;
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  validateTextInput(value: string) {
+    const regex = /^[a-zA-Z\s]+$/;
+    if (!regex.test(value)) {
+      console.log('Este campo solo debe contener letras');
+    }
   }
 
+  validateID(cardNumber: string) {
+    // Lógica de validación de cédula
+    const isValid = this.isForeigner ? true : cardNumber.length === 10;
+    if (!isValid) {
+      console.log('Cédula inválida');
+    }
+  }
+
+  togglePassportField() {
+    this.isForeigner = !this.isForeigner;
+  }
+
+  validatePhone(phoneNumber: string) {
+    const regex = /^[0-9]{10}$/; // Ejemplo para un teléfono de 10 dígitos
+    if (!regex.test(phoneNumber)) {
+      console.log('Número de teléfono inválido');
+    }
+  }
+  //VALIDA COORREO
+  validateEmail(email: string) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!regex.test(email)) {
+      console.log('Correo electrónico no válido');
+    }
+  }
+  //REGISTRA EL USUARIO
+  userRegister() {
+    // Validar campos vacíos
+    if (
+      !this.firstName ||
+      !this.lastName ||
+      (!this.cardNumber && !this.passportNumber) ||
+      !this.phoneNumber ||
+      !this.address ||
+      !this.city ||
+      !this.province ||
+      !this.career ||
+      !this.semester ||
+      !this.age ||
+      !this.gender ||
+      !this.weight ||
+      !this.height ||
+      !this.institutionalEmail ||
+      !this.dateOfBirth ||
+      !this.sede ||
+      !this.user_name ||
+      !this.email_user ||
+      !this.password_user ||
+      !this.password_user2 ||
+      !this.email_user_re
+    ) {
+      console.log('Por favor, complete todos los campos');
+      return;
+    }
+
+    // Validar contraseñas
+    if (!this.validatePassword(this.password_user, this.password_user2)) {
+      console.log('Errores en la validación de contraseñas');
+      return;
+    }
+
+    console.log('Registro exitoso');
+  }
+
+  validatePassword(password: string, confirmPassword?: string) {
+    const minLength = 6;
+    const hasNumber = /\d/;
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
+
+    if (
+      password.length < minLength ||
+      !hasNumber.test(password) ||
+      !hasSpecialChar.test(password)
+    ) {
+      console.log(
+        'La contraseña debe tener al menos 6 caracteres, incluir un número y un carácter especial'
+      );
+      return false;
+    }
+
+    if (confirmPassword && password !== confirmPassword) {
+      console.log('Las contraseñas no coinciden');
+      return false;
+    }
+
+    return true;
+  }
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
