0d8142cf3c982fab82db0db2d5dfe67e1440859d
Kevin Sanchez <kevinsan1835@gmail.com>
2024-09-05 20:46:17 -0500

    vistas de recuperar contrasena

--- src/app/Pages/Public/change-password/change-password.page.ts
+++ src/app/Pages/Public/change-password/change-password.page.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.page.scss'],
 })
 export class ChangePasswordPage implements OnInit {
+  clave : string ="";
+  clave2 : string ="";
+  mensaje : string ="";
+  
 
   constructor() { }
 
   ngOnInit() {
   }
+  vclave(){
+    if(this.clave == this.clave2){
+      this.mensaje = "";
+    }else{
+      this.mensaje = "Las claves no coinciden";
+    }
+
+  }
+  changePassword(){
+    
+  }
+  
 
 }
--- src/app/Pages/Public/password-recovery/password-recovery.page.ts
+++ src/app/Pages/Public/password-recovery/password-recovery.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from 'src/app/Services/auth/auth.service';
 
 @Component({
   selector: 'app-password-recovery',
@@ -6,12 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-recovery.page.scss'],
 })
 export class PasswordRecoveryPage implements OnInit {
+  email : string =""
 
-  constructor() { }
+  constructor(
+    public navCtrl: NavController,
+    public servicio:AuthService
+
+  ) { }
 
   ngOnInit() {
   }
-  verificarcorreo(){
+  checkEmail(){
+    let datos={
+      accion:'checkEmail',
+      email: this.email
+      
+    }
+    this.servicio.postData(datos).subscribe((res:any)=>{
+      if(res.estado==true)
+      {
+        //aqui debo crear el token y enviar al correo
+        
+        this.servicio.showToast(res.mensaje);
+
+       this.navCtrl.navigateForward('check-token');
+       
+      }
+      else{
+        this.servicio.showToast(res.mensaje);
+      }
+    })
     
   }
 
