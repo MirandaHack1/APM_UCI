4729ccdff069bc0891484dad0f1152c8d4c0077d
DevJhonny <miranda3791167@gmail.com>
2024-09-23 17:02:48 -0500

    VALIDACION DE RECUPERACION EN USER ROLÇ

--- src/app/Pages/Secure/edit-user-rol/edit-user-rol.page.ts
+++ src/app/Pages/Secure/edit-user-rol/edit-user-rol.page.ts
@@ -8,60 +8,94 @@ import { EmailValidator } from '@angular/forms';
   styleUrls: ['./edit-user-rol.page.scss'],
 })
 export class EditUserRolPage implements OnInit {
-  nombre: string="";
-  rol:string="";
-  codigo: string = "";
+  nombre: string = '';
+  rol: string = '';
+  codigo: string = '';
   usuario: any = {};
-  email: string = "";
-  emailrecuperacion: string = "";
-  constructor(
-    public authService: AuthService,
-    public navCtrl: NavController
-  ) {
-    this.authService.getSession('codigo').then((res:any)=>{
-      this.codigo=res;
+  email: string = '';
+  emailrecuperacion: string = '';
+  constructor(public authService: AuthService, public navCtrl: NavController) {
+    this.authService.getSession('codigo').then((res: any) => {
+      this.codigo = res;
       //this.txt=this.codigousu;
-      if(this.codigo){
+      if (this.codigo) {
         this.cargarDatosUsuario();
       }
     });
-   }
+  }
 
- 
-  ngOnInit() {
-  
+  ngOnInit() {}
+
+  //VADIDACION DE CORREO ELECTRONICO
+  validateEmail2(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      this.authService.showToast('Error: Correo electrónico no válido', true);
+      return false;
+    }
+    return true;
   }
 
   cargarDatosUsuario() {
     let datos = {
       accion: 'consultausuarioDATOS',
-      codigo: this.codigo
+      codigo: this.codigo,
     };
     this.authService.postData(datos).subscribe((res: any) => {
       if (res.estado === true) {
         this.usuario = res.datos[0];
-        this.nombre=this.usuario.nombre;
-        this.email=this.usuario.email;
-        this.emailrecuperacion=this.usuario.emailrecuperacion;
-        this.rol = this.usuario.rol;       
+        this.nombre = this.usuario.nombre;
+        this.email = this.usuario.email;
+        this.emailrecuperacion = this.usuario.emailrecuperacion;
+        this.rol = this.usuario.rol;
       } else {
         this.authService.showToast(res.mensaje, true);
       }
     });
   }
-  
 
- guardarCambios() {
+  // guardarCambios() {
+  //   // Validar correo electrónico general
+  //   if (!this.validateEmail2(this.email)) {
+  //     return; // Detener si el correo no es válido
+  //   }
+  //   let datos = {
+  //     accion: 'editarusuario',
+  //     codigo: this.codigo,
+  //     email: this.email,
+  //     emailrecuperacion: this.emailrecuperacion,
+  //     rol: this.rol,
+  //   };
+
+  //   console.log(datos); // Verifica que los datos sean correctos
+
+  //   this.authService.postData(datos).subscribe((res: any) => {
+  //     console.log(res);
+  //     if (res.estado === true) {
+  //       this.authService.showToast('Usuario actualizado correctamente');
+  //       this.navCtrl.back();
+  //     } else {
+  //       this.authService.showToast(res.mensaje, true);
+  //     }
+  //   });
+  // }
+  guardarCambios() {
+    // Validar correo electrónico general
+    if (!this.validateEmail2(this.emailrecuperacion)) {
+      this.authService.showToast('El correo electrónico no es válido', true);
+      return; // Detener si el correo no es válido
+    }
+  
     let datos = {
       accion: 'editarusuario',
-      codigo: this.codigo, 
+      codigo: this.codigo,
       email: this.email,
       emailrecuperacion: this.emailrecuperacion,
-      rol: this.rol 
+      rol: this.rol,
     };
-
+  
     console.log(datos); // Verifica que los datos sean correctos
-
+  
     this.authService.postData(datos).subscribe((res: any) => {
       console.log(res);
       if (res.estado === true) {
@@ -72,7 +106,8 @@ export class EditUserRolPage implements OnInit {
       }
     });
   }
-  cancelar(){
+  
+  cancelar() {
     this.navCtrl.back();
   }
 }
